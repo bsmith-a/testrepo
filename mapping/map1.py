563b18474c9d835b16f52420491a50a0dcd63712
bsmith-a <bsmith@gsuite.amplex.net>
2021-06-10 12:45:02 -0400

    finished web map

--- mapping/map1.py
+++ mapping/map1.py
@@ -24,21 +24,34 @@ Height: %s m
 """
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(
         html=html % (name, name, el),
         width=200,
         height=100)
-    fg.add_child(
+    fgv.add_child(
         folium.CircleMarker(
             location=[lt, ln],
+            radius=7,
             popup=folium.Popup(iframe),
-            icon=folium.Circle(color=color_maker(el))
+            fill_color=color_maker(el),
+            color='black',
+            fill_opacity=0.8,
+            weight=2
         )
     )
 
-map.add_child(fg)
+fgp = folium.FeatureGroup(name="Population")
 
+fgp.add_child(folium.GeoJson(data=open("world.json", "r", encoding='utf-8-sig').read(),
+                             style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 1000000
+                             else 'yellow' if x['properties']['POP2005'] < 100000000
+                             else 'orange' if x['properties']['POP2005'] < 1000000000
+                             else 'red'}))
+
+map.add_child(fgp)
+map.add_child(fgv)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
